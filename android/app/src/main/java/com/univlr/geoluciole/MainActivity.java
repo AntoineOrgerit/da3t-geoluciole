9988cc5e794b0435f66be76a89a8993dc434a51e
Antoine Orgerit <orgerit.antoine@gmail.com>
2020-01-10 14:59:24 +0100

    Ajout d'un swipe sur les fragments visuels.

--- android/app/src/main/java/com/univlr/geoluciole/MainActivity.java
+++ android/app/src/main/java/com/univlr/geoluciole/MainActivity.java
@@ -1,28 +1,93 @@
 package com.univlr.geoluciole;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.univlr.geoluciole.adapter.ViewPagerAdapter;
+import com.univlr.geoluciole.ui.dashboard.DashboardFragment;
+import com.univlr.geoluciole.ui.home.HomeFragment;
+import com.univlr.geoluciole.ui.notifications.NotificationsFragment;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private BottomNavigationView navView;
+    private MenuItem prevMenuItem;
+    private ViewPager viewPager;
+
+    private HomeFragment homeFragment;
+    private DashboardFragment dashboardFragment;
+    private NotificationsFragment notificationsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        navView = findViewById(R.id.nav_view);
+        prevMenuItem = null;
+        viewPager = findViewById(R.id.viewpager);
+        navView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.navigation_dashboard:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.navigation_notifications:
+                        viewPager.setCurrentItem(2);
+                        break;
+                }
+                return false;
+            }
+        });
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // do nothing
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    navView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d(TAG, "onPageSelected: " + position);
+                navView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = navView.getMenu().getItem(position);
+
+            }
+
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // do nothing
+            }
+        });
+
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        homeFragment=new HomeFragment();
+        dashboardFragment =new DashboardFragment();
+        notificationsFragment = new NotificationsFragment();
+        adapter.addFragment(homeFragment);
+        adapter.addFragment(dashboardFragment);
+        adapter.addFragment(notificationsFragment);
+        viewPager.setAdapter(adapter);
     }
 
 }
