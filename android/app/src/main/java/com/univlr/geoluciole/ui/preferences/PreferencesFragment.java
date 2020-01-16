f580fadd6c5bf38b6635b760921d47736f0e04fc
Florian Bertonnier <florian.bertonnier@etudiant.univ-lr.fr>
2020-01-16 15:52:49 +0100

    Fnc : Liaison entre le bouton cgu et l'activité cgu.

--- android/app/src/main/java/com/univlr/geoluciole/ui/preferences/PreferencesFragment.java
+++ android/app/src/main/java/com/univlr/geoluciole/ui/preferences/PreferencesFragment.java
@@ -1,33 +1,35 @@
 package com.univlr.geoluciole.ui.preferences;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import androidx.annotation.Nullable;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import com.univlr.geoluciole.CguActivity;
 import com.univlr.geoluciole.R;
 
 public class PreferencesFragment extends Fragment {
 
-    private PreferencesViewModel preferencesViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        preferencesViewModel =
+        PreferencesViewModel preferencesViewModel =
                 ViewModelProviders.of(this).get(PreferencesViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_preferences, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_preferences);
-        preferencesViewModel.getText().observe(this, new Observer<String>() {
+        final View root = inflater.inflate(R.layout.fragment_preferences, container, false);
+
+        Button button = root.findViewById(R.id.button_license_agreement);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                startActivity(new Intent(root.getContext(), CguActivity.class));
             }
-        });*/
+        });
+
         return root;
     }
 }
