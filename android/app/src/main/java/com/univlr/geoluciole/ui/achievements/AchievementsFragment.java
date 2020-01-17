4bdbb7b678d9b33441e056838090e10a563be7eb
lisa.fougeron <lisa.fougeron@etudiant.univ-lr.fr>
2020-01-17 20:35:52 +0100

    Fnc : Mise en place de la logique pour la gestion des badges Import et instanciation des badges depuis un fichier json

--- android/app/src/main/java/com/univlr/geoluciole/ui/achievements/AchievementsFragment.java
+++ android/app/src/main/java/com/univlr/geoluciole/ui/achievements/AchievementsFragment.java
@@ -1,23 +1,30 @@
 package com.univlr.geoluciole.ui.achievements;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
 import com.univlr.geoluciole.R;
+import com.univlr.geoluciole.model.badge.BadgeManager;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AchievementsFragment extends Fragment {
 
     private AchievementsViewModel achievementsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         achievementsViewModel =
                 ViewModelProviders.of(this).get(AchievementsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_achievements, container, false);
@@ -28,6 +35,10 @@ public class AchievementsFragment extends Fragment {
                 textView.setText(s);
             }
         });*/
+        BadgeManager badgeManager = BadgeManager.getInstance(root.getContext()); // root.getContext() dans le fragment
+        badgeManager.instanciateObjFromJson();
+        badgeManager.displayBadgesList();
         return root;
     }
+
 }
